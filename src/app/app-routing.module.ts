40631e85a9ceee9e00eb9a32b3439942cb25b79e
thewingson <almatrakh97@gmail.com>
2019-06-05 23:42:04 +0600

    rout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import {HomeStateComponent} from './component/home-state/home-state.component';
 import {GlobalStateComponent} from './component/global-state/global-state.component';
 
 const routes: Routes = [
+    {path: '', component: GlobalStateComponent},
     {path: 'home', component: HomeStateComponent, data: {animation: 'isRight'}},
     {path: 'global', component: GlobalStateComponent, data: {animation: 'isLeft'}},
+
 ];
 
 @NgModule({
