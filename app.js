ba82053e575e3ef3bd47a541ba60b78e0d277557
thewingson <almatrakh97@gmail.com>
2019-06-06 01:02:54 +0600

    new configs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-const compression = require('compression');
+const compression = r
 
 const app = express();
 
